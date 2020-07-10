12a7c9edde62f9bbfd1c048ba8f2436e468692fe
eking2 <eking2@uci.edu>
2020-07-09 22:21:13 -0700

    upto replace b

--- run_blast.py
+++ run_blast.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python3
 
-from Bio import SeqIO, SearchIO, Entrez
+from Bio import AlignIO, SeqIO, SearchIO, Entrez, motifs, PDB
+from Bio.Alphabet import IUPAC, Gapped
 from Bio.Blast import NCBIWWW
+from Bio.Seq import Seq
+from io import StringIO
 import numpy as np
 import pandas as pd
 from pathlib import Path
-import shlex
-import subprocess
+import os
 import re
+import requests
+from scipy import stats
+from sklearn.preprocessing import MinMaxScaler
+
 
 # run blast on sequence to identify homologs
 # filter and download hits
@@ -143,8 +149,9 @@ def mafft_align(hits_fasta):
         hits_fasta (str) : fasta file with template and hits
     """
 
-    cmd = 'mafft'
-
+    name = hits_fasta.split('.')[0].split('/')[-1]
+    cmd = f'mafft {hits_fasta} > outputs/{name}_mafft.fasta'
+    os.system(cmd)
 
 
 def clean_alignment(mafft_align):
@@ -155,4 +162,162 @@ def clean_alignment(mafft_align):
         mafft_align (str) : fasta file with mafft aligned template and hits
     """
 
+    records = AlignIO.read(mafft_align, 'fasta')
+    template = records[0].seq
+
+    # get gap positions
+    gaps = [i for i, char in enumerate(str(template)) if char == '-']
+
+    # loop through each seq and remove gap cols
+    for record in records:
+        seq = str(record.seq)
+
+        # remove unknown amino acids (problems due to inclusion of MSE, CYX)
+        seq = seq.replace('X', '-')
+        clean_seq = ''.join([seq[i] for i in range(len(seq)) if i not in gaps])
+        record.seq = Seq(clean_seq, Gapped(IUPAC.protein))
+
+    # save clean fasta
+    name = mafft_align.split('.')[0].split('/')[-1].split('_')[0]
+    AlignIO.write(records, f'outputs/{name}_clean.fasta','fasta')
+
+
+def msa_to_freq(clean_fasta):
+
+    """Count frequency of amino acids at each position in MSA.
+    
+    Args:
+        clean_fasta (str) : fasta file with cleaned mafft alignment
+
+    Returns:
+        counts_df (dataframe) : dataframe with shape [position, amino acid] listing counts
+    """
+
+    records = AlignIO.read(clean_fasta, 'fasta')
+
+    # to motif
+    m = motifs.create([Seq(str(record.seq), Gapped(IUPAC.protein)) for record in records])
+
+    # counts [position, amino acid]
+    df = pd.DataFrame(m.counts).reset_index()
+    df['index'] += 1
+    df.rename(columns = {'index' : 'position'}, inplace=True)
+    df.set_index('position', drop=True, inplace=True)
+
+    name = clean_fasta.split('.')[0].split('/')[-1].replace('_clean', '')
+    df.to_csv(f'outputs/{name}_msa_freq.csv')
+
+
+def shannon_entropy(msa_freq):
+
+    """Calculate position shannon entropy. Higher entropy equals less consevation.
+
+    Args:
+        msa_freq (str) : filename for msa frequency csv
+    """
+
+    df = pd.read_csv(msa_freq)
+
+    # shannon ent
+    se = stats.entropy(df.T, base=2)
+
+    # convert to z-score, percentile, min-max
+    zscore = stats.zscore(se)
+    ppf = stats.norm.cdf(zscore)
+
+    # reshape for scipy, then flatten for pd
+    scaled = MinMaxScaler().fit_transform(se.reshape(-1, 1)).reshape(-1)
+
+    out = pd.DataFrame({'shannon' : se,
+                        'z_score' : zscore,
+                        'percentile' : ppf,
+                        'min_max' : scaled})
+
+    out['position'] = range(1, len(out) + 1)
+    out = out[['position', 'shannon', 'z_score', 'percentile', 'min_max']]
+
+    name = msa_freq.split('.')[0].split('/')[-1].replace('_msa_freq', '')
+    out.to_csv(f'outputs/{name}_entropy.csv', index=False)
+    
+
+def save_chain(content, chain):
+
+    """Save single chain from pdb, no hetatms.
+
+    Args:
+        content (str) : pdb text
+        chain (str) : subunit chain to keep
+    """
+
+    out = []
+    for line in content:
+        if line.startswith('ATOM') and line.split()[4] == chain:
+            out.append(line)
+
+    saved_chain = '\n'.join(out)
+
+    return saved_chain
+
+
+def download_pdb(pdb):
+
+    """Download pdb chain from rcsb, remove hetatms.
+
+    Args:
+        pdb (str) : 4 letter pdb id
+    """
+
+    url = f'https://files.rcsb.org/download/{pdb}.pdb' 
+    r = requests.get(url)
+    assert r.status_code == 200, f'invalid request {pdb}'
+    content = r.text.splitlines()
+
+    return content
+
+
+def replace_b(structure, new_b):
+
+    """Replace b-factors residue wise.
+
+    Args:
+        structure (BioPython Structure) : pdb
+        new_b (array) : new b-factor values to map onto pdb
+    """
+
     pass
+
+
+def map_se_on_pdb(pdb, chain, entropy, col):
+
+    """Map entropy onto structure.
+
+    Args:
+        pdb (str) : 4 letter pdb id
+        chain (str) : subunit chain to keep
+        entropy (str) : entropy csv with values to map
+        col (str) : feature to map, choices are shannon, z_score, percentile, min_max
+    """
+
+    # download pdb
+    pdb_content = download_pdb(pdb)
+    chain = save_chain(pdb_content, chain)
+
+    # load into structure
+    parser = PDBParser()
+    structure = parser.get_structure('struct', StringIO(chain))
+
+    # new values to map
+    df = pd.read_csv(entropy)
+    new_b = df[col]
+
+    # replace
+
+    
+    # save
+
+
+
+    
+
+
+
